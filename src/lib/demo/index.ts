c118de1d064f016aa8a70b15d9898931429648b7
Qiujiajun <qiujiajun2024@163.com>
2026-05-05 00:41:00 +0800

    feat: scaffold demo mode data

---
+++ src/lib/demo/index.ts
@@ -0,0 +1,20 @@
+export {
+  demoAiSummaries,
+  demoCommuteSummaries,
+  demoListings,
+  demoModeData,
+  demoModeMeta,
+  demoScoreBreakdowns,
+  demoWorkLocations,
+} from "./demo-data";
+
+export type {
+  DemoAiSummary,
+  DemoCommuteMode,
+  DemoCommuteSummary,
+  DemoListing,
+  DemoListingStatus,
+  DemoModeMeta,
+  DemoScoreBreakdown,
+  DemoWorkLocation,
+} from "./demo-data";
